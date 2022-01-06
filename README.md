45d908f8d74de1fe56b554cbcf57e97907a1d584
David Rodrigues <davidmiguelfr@tecnico.ulisboa.pt>
2022-01-06 11:11:51 +0000

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Project
+Steps to activate:
+1. Install ZooKeeper (Screenshots in Discord)
+2. Start ZooKeeper by going to zookeper/bin and executing `./zkServer.sh start`
+3. `mvn install` in root directory
+4. `mvn compile exec:java` in Server directory first, followed by the same command in Client directory
---
+++ Server/src/main/java/pt/tecnico/Server/ServerMain.java
@@ -0,0 +1,86 @@
+package pt.tecnico.Server;
+
+import io.grpc.BindableService;
+import io.grpc.ServerBuilder;
+import io.grpc.Server;
+import pt.ulisboa.tecnico.sdis.zk.ZKNaming;
+import pt.ulisboa.tecnico.sdis.zk.ZKNamingException;
+
+public class ServerMain {
+
+	/** ZooKeeper host name. */
+	private static String zooHost;
+	/** ZooKeeper host port. */
+	private static int zooPort;
+	/** ZooKeeper helper object. */
+	private static ZKNaming zkNaming = null;
+
+	/** ZooKeeper path where information about the server will be published. */
+	private static String path;
+	/** Server host name. */
+	private static String host;
+	/** Server host port. */
+	private static int port;
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(ServerMain.class.getSimpleName());
+
+		// Print received arguments.
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
+
+		// Check arguments.
+		if (args.length < 5) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s zooHost zooPort path host port%n", ServerMain.class.getName());
+			return;
+		}
+
+		zooHost = args[0];
+		zooPort = Integer.valueOf(args[1]);
+		path = args[2];
+		host = args[3];
+		port = Integer.valueOf(args[4]);
+
+		final BindableService impl = new HelloWorldServiceImpl();
+
+		// Create a new server to listen on port.
+		Server serverMain = ServerBuilder.forPort(port).addService(impl).build();
+		// Start the server.
+		serverMain.start();
+		// Server threads are running in the background.
+
+		// Register on ZooKeeper.
+		System.out.println("Contacting ZooKeeper at " + zooHost + ":" + zooPort + "...");
+		zkNaming = new ZKNaming(zooHost, Integer.toString(zooPort));
+		System.out.println("Binding " + path + " to " + host + ":" + port + "...");
+		zkNaming.rebind(path, host, Integer.toString(port));
+		// Use hook to register a thread to be called on shutdown.
+		Runtime.getRuntime().addShutdownHook(new Unbind());
+
+		System.out.println("Server started and awaiting requests on port " + port);
+		// Do not exit the main thread. Wait until server is terminated.
+		serverMain.awaitTermination();
+	}
+
+	/** 
+	 * Unbind class unbinds replica from ZKNaming after interruption.
+	 */
+	static class Unbind extends Thread {
+		public void run() {
+			if (zkNaming != null) {
+				try {
+					System.out.println("Unbinding " + path + " from ZooKeeper...");
+					zkNaming.unbind(path, host, String.valueOf(port));
+				}
+			   	catch (ZKNamingException e) {
+					System.err.println("Could not close connection with ZooKeeper: " + e);
+					return;
+				}
+			}
+		}
+	}
+
+}
